55203ecb5dcab07ef5fd204b1400fd14d7a6d8c5
unknown <howie@howie-HP.(none)>
2013-03-17 16:07:36 +0100

    First commit to repository

---
+++ StateMachine/StateMachinePartial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StateMachine
+{
+    public partial class StateMachine
+    {
+        private string xml_file;
+
+        public StateMachine(string file)
+        {
+            xml_file = file;
+        }
+    }
+}
---
+++ StateMachineConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StateMachineConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Projects\Fun\StateMachine\StateMachineGenerator\StateMachineConsole\scxml\SCStopwatch.xml";
+            StateMachine.StateMachine sm = new StateMachine.StateMachine(file);
+            
+            string code = sm.TransformText();
+            System.IO.File.WriteAllText(@"C:\temp\GeneratedCode.cs", code);
+
+        }
+    }
+}
